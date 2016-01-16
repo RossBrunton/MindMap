c450c6aeed0d33336de4d39866e77aaae7b96287
Ross Brunton <bruntonross@gmail.com>
2016-01-16 17:40:18 +0000

    Can render a basic diagram
    Now just need to style it... And also figure out what the hell is going
    on with the edges. o_O

--- es6/Renderer.js
+++ es6/Renderer.js
@@ -19,7 +19,7 @@ load.provide("mm.Renderer", (function() {
 	 * @param {HTMLElement} node The node to use. Must be a plain HTMLElement, not a JQuery object.
 	 */
 	return class Renderer {
-		constructor(node, types, objects) {
+		constructor(node, types) {
 			/** The HTML node
 			 * 
 			 * @type HTMLElement
@@ -37,17 +37,75 @@ load.provide("mm.Renderer", (function() {
 			 */
 			this._id = this.node.id;
 			
+			/** The joint.js graph used for this node
+			 * 
+			 * Null until it is inited.
+			 * 
+			 * @type ?joint.dia.Graph
+			 * @private
+			 */
+			this._graph = null;
+			/** The joint.js paper used for this node
+			 * 
+			 * Null until it is inited
+			 * 
+			 * @type ?joint.dia.Paper
+			 * @private
+			 */
+			this._paper = null;
+			
+			
 			this.init();
 		}
 		
 		rerender(objects) {
-			console.log(objects);
+			// Set dimensions
+			this._paper.setDimensions(objects.canvas.width, objects.canvas.height);
+			this._paper.setOrigin(objects.canvas.offsetX, objects.canvas.offsetY);
+			
+			// Now do the nodes
+			let nodeIds = new Map();
+			
+			for(let n of objects.nodes) {
+				let rect = new joint.shapes.basic.Rect({
+					position:{x:n.x, y:n.y},
+					size:{width:100, height:30},
+					attrs:n.type.nodeAttr
+				});
+				
+				this._graph.addCell(rect);
+				nodeIds.set(n.id, rect);
+			}
+			
+			// Edges
+			for(let e of objects.edges) {
+				let link = new joint.dia.Link({
+					source: {id:nodeIds.get(e.origin).id},
+					target: {id:nodeIds.get(e.dest).id},
+					attr:e.type.attr,
+					vertices:e.points.map(([x, y]) => ({x:x, y:y}))
+				});
+				
+				this._graph.addCell(link);
+			}
 		}
 		
 		/** Inits the element, by creating the needed elements inside it */
 		init() {
 			this.node.innerHTML = _TEMPLATE;
 			this.node.classList.add("_mm_root");
+			
+			// Set up jointjs
+			let graph = this._graph = new joint.dia.Graph();
+			
+			let paper = this._paper = new joint.dia.Paper({
+				el:$(`#${this._id} ._mm_paper`),
+				model:graph,
+				gridSize:1,
+				width:0,
+				height:0,
+				interactive:true//false
+			});
 		}
 	};
 })());
