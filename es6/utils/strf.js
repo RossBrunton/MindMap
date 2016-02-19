8f350ae9d4b169a1c8826fc30409fe56b6693d17
Ross Brunton <bruntonross@gmail.com>
2016-02-19 15:57:22 +0000

    Some formatting functions

--- es6/utils/strf.js
+++ es6/utils/strf.js
@@ -1,6 +1,17 @@
 "use strict";
 
 load.provide("mm.utils.strf", (function() {
+	let _fns = {
+		link: (x, a, type) => {
+			if(type == "url") return `<a href='${encodeURI(x)}'>${x}</a>`;
+			if(type == "email") return `<a href='mailto:${encodeURI(x)}'>${x}</a>`;
+			return x;
+		},
+		singleLine: (x) => {return x.split("\n")[0]},
+		cap: (x, arg) => {return x.substring(0, arg)},
+		cape: (x, arg) => {return x.length >= arg ? x.substring(0, arg-3) + "..." : x},
+	};
+	
 	/** Formats a string, given a template and an objects node
 	 * 
 	 * The format is documented in docs/formatting.
@@ -9,35 +20,45 @@ load.provide("mm.utils.strf", (function() {
 	 * @param {mm.structs.ObjectNode} node The object node to format.
 	 * @return {string} The formatted string.
 	 */
-	return function(template, node) {
+	let strf = function(template, node) {
 		return template.replace(/{(.*?)}/g, (t) => {
 			t = t.substring(1, t.length-1).trim();
 			
 			if("{}".includes(t)) return t;
 			
 			if(t.includes(":")) {
-				//TODO
-			}
-			
-			let f = node.getFieldType(t);
-			if(!f) return "(null)";
-			let v = node.fields[t];
-			
-			switch(f.type) {
-				case "text":
-				case "blocktext":
-				case "url":
-				case "email":
-					return v;
-					break;
-					
-				case "date":
-					return "(date not supported)";
-					break;
+				let [fn, rhs] = t.split(":");
+				let [varname, arg] = rhs.split(",");
 				
-				default:
-					return "(unknown type)";
+				let f = node.getFieldType(varname);
+				if(!f) return "(null)";
+				let v = node.fields[varname];
+				
+				return _fns[fn](v, arg, f.type);
+			}else{
+				let f = node.getFieldType(t);
+				if(!f) return "(null)";
+				let v = node.fields[t];
+				
+				switch(f.type) {
+					case "text":
+					case "blocktext":
+					case "url":
+					case "email":
+						return v;
+						break;
+						
+					case "date":
+						return "(date not supported)";
+						break;
+					
+					default:
+						return "(unknown type)";
+				}
 			}
 		});
 	};
+	
+	window.strf = strf;
+	return strf;
 })());
