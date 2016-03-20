dc1ca7dfa7bd72d38e73abac509725c0a80d0913
Ross Brunton <bruntonross@gmail.com>
2016-03-20 16:01:10 +0000

    Documented the edit help thing

--- es6/interactions/EditHelp.js
+++ es6/interactions/EditHelp.js
@@ -3,9 +3,16 @@
 load.provide("mm.interactions.EditHelp", (function() {
 	let Interaction = load.require("mm.interactions.Interaction");
 	
+	/** Interactor for showing and hiding the help screen
+	 * 
+	 * @extends mm.Interaciton
+	 */
 	return class EditHelp extends Interaction {
 		async addCanvas(renderer, node) {
+			// Show it when the button is clicked
 			$(node).find(".mm-help-button").click((e) => $(node).find(".mm-edit-help").removeClass("hidden"));
+			
+			// And hide it when the panel is clicked
 			$(node).find(".mm-edit-help").click((e) => $(node).find(".mm-edit-help").addClass("hidden"));
 		}
 	};
