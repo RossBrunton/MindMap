65e5d6ceab2bc0c7a90672c98f970b72b267aee3
Ross Brunton <bruntonross@gmail.com>
2016-04-11 14:08:21 +0100

    Documented Pan

--- es6/interactions/Pan.js
+++ es6/interactions/Pan.js
@@ -3,15 +3,27 @@
 load.provide("mm.interactions.Pan", (function() {
 	let Interaction = load.require("mm.interactions.Interaction");
 	
+	/** Handles click draging around the diagram
+	 * 
+	 * Basically, listens for mouse movements and updates the scroll location of the diagram.
+	 * 
+	 * @extends mm.Interaction
+	 */
 	return class Pan extends Interaction {
 		async addCanvas(renderer, html) {
 			let mouseDown = false;
+			
+			// Current mouse location, used to check how much the mouse has moved since the last mousemove event
 			let cx = 0;
 			let cy = 0;
 			
 			html.addEventListener("mousedown", (e) => {
+				// Don't bother dragging if we aren't using the left click
 				if(e.button != 0) return;
+				
+				// ... Or if we have clicked something else and aren't in the editor
 				if(!$(e.target).hasClass("mm-background-grid") && this._editor) return;
+				
 				mouseDown = true;
 				cx = e.clientX;
 				cy = e.clientY;
